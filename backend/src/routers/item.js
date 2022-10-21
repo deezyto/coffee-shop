c2abfb1f196f41e531ba465465df3979b0bbec7c
Andrey <asvidovskiy@gmail.com>
2022-10-21 21:36:32 +0300

    update router item in admin for include filter results

--- backend/src/routers/item.js
+++ backend/src/routers/item.js
@@ -4,7 +4,19 @@ const Item = require('../models/admin/item');
 
 router.get('/coffee', async (req, res) => {
   try {
-    const item = await Item.find({});
+    const sort = {};
+    if (req.query.sortBy) {
+      const parts = req.query.sortBy.split(':');
+      sort[parts[0]] = parts[1] === 'asc' ? 1 : -1;
+    }
+    const item = await Item.find({}, null, {
+      limit: parseInt(req.query.limit),
+      skip: parseInt(req.query.skip),
+      sort
+    })
+    if (!item.length) {
+      res.status(404).send();
+    }
     res.send(item);
   } catch {
     res.status(500).send()
