e599d1797aa097ed2916ff8fb467dfba0fd3bba8
Andrey <asvidovskiy@gmail.com>
2022-10-26 06:09:45 +0300

    complete router for get categories and subcategories

--- backend/src/middleware/middleware.category.js
+++ backend/src/middleware/middleware.category.js
@@ -1,15 +1,31 @@
+const Category = require('../models/model.category');
 
 const category = async (req, res, next) => {
   try {
-    console.log(req.params, 'category');
-    if (!user) {
-      throw new Error();
+    const arraysSubCategoriesSlug = req.params['0'].split('/').filter(item => item.length !== 0);
+    const category = await Category.findOne({slug: arraysSubCategoriesSlug[0]});
+    if (!category || category.parentCategories.length) {
+      res.status(404).send();
     }
-    if (user.role === 'admin') {
-      req.admin = true;
-    }
-    req.token = token;
-    req.user = user;
+
+    await (async function() {
+      let i = 0;
+      for await (let slug of arraysSubCategoriesSlug) {
+        if (i > 0) {
+          const subCategories = await Category.findOne({slug});
+          if (!subCategories) {
+            return res.status(404).send();
+          }
+          const prevCategory = await Category.findOne({slug: arraysSubCategoriesSlug[i - 1]});
+          if (!subCategories.parentCategories.find(item => item.toString() === prevCategory._id.toString())) {
+            return res.status(404).send();
+          }
+        }
+        i++;
+      }
+    })();
+
+    req.category = category;
     next();
   } catch (e) {
     res.status(401).send('Please authenticate');
--- backend/src/routers/get/routers.get.item.js
+++ backend/src/routers/get/routers.get.item.js
@@ -1,37 +1,15 @@
 const express = require('express');
 const router = new express.Router();
-const auth = require('../../middleware/middleware.auth');
 const category = require('../../middleware/middleware.category');
 const Item = require('../../models/model.item');
 const Category = require('../../models/model.category');
+const categoryMiddleware = require('../../middleware/middleware.category');
 const { compareSync } = require('bcryptjs');
 
 //get items from category
 //get subcategory from category
-router.get('/:category', async (req, res) => {
-
+router.get('*', categoryMiddleware, async (req, res) => {
   try {
-    console.log(req.params)
-    const slug = req.params['0'].split('/').filter(item => item.length !== 0);
-
-    console.log(slug)
-    for (let i = slug.length; i > 0; i--) {
-
-    }
-
-    slug.forEach(async (slug, i) => {
-      const category = await Category.findOne({slug});
-
-      if (!category) {
-        res.status(404).send();
-      }
-    })
-
-    const category = await Category.findOne({slug: slug[slug.length - 1]});
-    if (!category) {
-      res.status(404).send();
-    }
-
     const sort = {};
     if (req.query.sortBy) {
       const parts = req.query.sortBy.split(':');
@@ -39,27 +17,14 @@ router.get('/:category', async (req, res) => {
     }
 
     const items = await Item.find({
-      '_id': {$in: category.items}
+      '_id': {$in: req.category.items}
     }, null, {
       limit: parseInt(req.query.limit),
       skip: parseInt(req.query.skip),
       sort
     });
-    //const itemsFromCategories = await Category.findById(category._id).populate("subCategories");
-    //const itemsFromCategories = await category.populate("items");
-    res.send({items, subCategory: category.subCategories});
-  } catch {
-    res.status(500).send()
-  }
-})
 
-//get items from subcategory
-router.get('/:category/:subcategory', async (req, res) => {
-  try {
-    const category = await Category.findOne({slug: req.params.category});
-    const items = await Item.find({categories: category._id});
-    const itemsFromSubCategories = await Category.findById(category._id).populate("items");
-    res.send(itemsFromSubCategories);
+    res.send({items, subCategory: req.category.subCategories});
   } catch {
     res.status(500).send()
   }
