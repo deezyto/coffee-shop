f8c8906fd3c85c11b5539864751d54fd357258e5
Andrey <asvidovskiy@gmail.com>
2022-10-27 21:43:21 +0300

    complete route for delete category

--- backend/src/routers/delete/routers.delete.item.js
+++ backend/src/routers/delete/routers.delete.item.js
@@ -2,14 +2,26 @@ const express = require('express');
 const router = new express.Router();
 const auth = require('../../middleware/middleware.auth');
 const Item = require('../../models/model.item');
+const Category = require('../../models/model.category');
 
-router.delete('/coffee/:id', auth, async (req, res) => {
+router.delete('/delete/item/:id', auth, async (req, res) => {
   try {
     if (req.admin) {
       const item = await Item.findById(req.params.id);
       if (!item) {
         res.status(404).send();
       }
+
+      if (item.parentCategories.length) {
+        item.parentCategories.forEach(async category => {
+          await Category.findByIdAndUpdate(
+            category,
+            {$pull: {items: req.params.id } },
+            {new: true, useFindAndModify: false}
+          )
+        });
+      }
+
       item.remove();
       res.send(item);
     }
