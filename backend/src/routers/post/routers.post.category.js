8bb5dc6d96e29ed18d70d9f1a90d62d8a0107208
Andrey <asvidovskiy@gmail.com>
2022-11-28 15:01:15 +0200

    refactoring router.post for item and category

--- backend/src/routers/post/routers.post.category.js
+++ backend/src/routers/post/routers.post.category.js
@@ -5,59 +5,59 @@ const Category = require('../../models/model.category');
 
 router.post('/create/category', auth, async (req, res) => {
   try {
-    if (req.admin) {
-      const checkSlug = await Category.findOne({ slug: req.body.slug });
-
-      if (checkSlug) {
-        res.status(400).send({ err: `Slug ${req.body.slug} is available. Please choose another unique slug` });
-      }
-
-      //create new category
-      const objCategory = {
-        title: req.body.title,
-        slug: req.body.slug,
-        description: req.body.description,
-        html: req.body.html,
-        metaTags: req.body.metaTags,
-        mainCategory: req.body.mainCategory
-      }
-      if (req.body.mainCategory) {
-        let categoryUrl = [];
-        const mainCategory = await Category.findById(req.body.mainCategory);
-        if (!mainCategory) {
-          return res.status(400).send({ err: 'main category not found' });
-        }
-        let currentMainCategory = mainCategory;
-
-        while (currentMainCategory.mainCategory) {
-          categoryUrl.unshift(currentMainCategory.slug);
-          currentMainCategory = await Category.findById(currentMainCategory.mainCategory);
-        }
-        categoryUrl.unshift('/' + currentMainCategory.slug);
-        categoryUrl.push(req.body.slug);
-
-        objCategory.url = categoryUrl.join('/');
-        const category = new Category(objCategory);
-        await category.save();
-
-        await Category.findByIdAndUpdate(
-          mainCategory._id,
-          { $addToSet: { subCategories: category._id } },
-          { new: true, useFindAndModify: false }
-        );
-
-        res.status(201).send(category);
-      } else {
-        objCategory.url = '/' + req.body.slug;
-        const category = new Category(objCategory);
-        await category.save();
-        res.status(201).send(category);
-      }
-
+    if (!req.admin) {
+      return res.status(403).send();
     }
-    res.status(403).send();
+
+    const checkSlug = await Category.findOne({ slug: req.body.slug });
+
+    if (checkSlug) {
+      return res.status(400).send({ err: `Slug ${req.body.slug} is available. Please choose another unique slug` });
+    }
+
+    //create new category
+    const objCategory = {
+      title: req.body.title,
+      slug: req.body.slug,
+      description: req.body.description,
+      html: req.body.html,
+      metaTags: req.body.metaTags,
+      mainCategory: req.body.mainCategory
+    }
+    if (req.body.mainCategory) {
+      let categoryUrl = [];
+      const mainCategory = await Category.findById(req.body.mainCategory);
+      if (!mainCategory) {
+        return res.status(400).send({ err: 'main category not found' });
+      }
+      let currentMainCategory = mainCategory;
+
+      while (currentMainCategory.mainCategory) {
+        categoryUrl.unshift(currentMainCategory.slug);
+        currentMainCategory = await Category.findById(currentMainCategory.mainCategory);
+      }
+      categoryUrl.unshift('/' + currentMainCategory.slug);
+      categoryUrl.push(req.body.slug);
+
+      objCategory.url = categoryUrl.join('/');
+      const category = new Category(objCategory);
+      await category.save();
+
+      await Category.findByIdAndUpdate(
+        mainCategory._id,
+        { $addToSet: { subCategories: category._id } },
+        { new: true, useFindAndModify: false }
+      );
+
+      res.status(201).send(category);
+    } else {
+      objCategory.url = '/' + req.body.slug;
+      const category = new Category(objCategory);
+      await category.save();
+      return res.status(201).send(category);
+    }
   } catch (e) {
-    res.status(400).send({ err: e });
+    return res.status(500).send({ err: 'Please check required fields', message: e });
   }
 });
 
--- backend/src/routers/post/routers.post.item.js
+++ backend/src/routers/post/routers.post.item.js
@@ -25,76 +25,77 @@ const addCategoryToItem = async function (categoryesIds = [], itemId = '') {
 //create item in category or subcategory
 router.post('/create/item', auth, async (req, res) => {
   try {
-    if (req.admin) {
-      //search item on slug
-      const checkSlug = await Item.findOne({ slug: req.body.slug });
-
-      if (checkSlug) {
-        return res.status(400).send({ err: `Slug ${req.body.slug} is available. Please choose another unique slug` });
-      }
-
-      const mainCategory = await Category.findById(req.body.mainCategory);
-      if (!mainCategory) {
-        return res.status(400).send({ err: 'main category not found' });
-      }
-
-      if (req.body.parentCategories) {
-        if (!Array.isArray(req.body.parentCategories)) {
-          return res.status(400).send({ err: 'Parent category must be a array type' });
-        }
-        for await (let id of req.body.parentCategories) {
-          const category = await Category.findById(id);
-          if (!category) {
-            return res.status(400).send({ err: `Parent category ${id} not fount` });
-          }
-        }
-        req.body.parentCategories.push(mainCategory._id);
-      }
-
-      if (!req.body.parentCategories) {
-        req.body.parentCategories = [mainCategory._id];
-      }
-
-      //create new item
-      let itemUrl = [];
-      let currentMainCategory = mainCategory;
-
-      while (currentMainCategory.mainCategory) {
-        itemUrl.unshift(currentMainCategory.slug);
-        currentMainCategory = await Category.findById(currentMainCategory.mainCategory);
-      }
-
-      itemUrl.unshift('/' + currentMainCategory.slug);
-      itemUrl.push(req.body.slug);
-
-      const objItem = {
-        title: req.body.title,
-        slug: req.body.slug,
-        description: req.body.description,
-        html: req.body.html,
-        metaTags: req.body.metaTags,
-        mainCategory: req.body.mainCategory,
-        url: itemUrl.join('/')
-      }
-
-      const item = new Item(objItem);
-
-      await item.save();
-
-      await Category.findByIdAndUpdate(
-        mainCategory._id,
-        { $addToSet: { mainItems: item._id } },
-        { new: true, useFindAndModify: false }
-      );
-
-      addItemToCategory(req.body.parentCategories, item._id);
-      addCategoryToItem(req.body.parentCategories, item._id);
-
-      res.status(201).send(item);
+    if (!req.admin) {
+      return res.status(403).send();
     }
-    res.status(403).send();
+
+    //search item on slug
+    const checkSlug = await Item.findOne({ slug: req.body.slug });
+
+    if (checkSlug) {
+      return res.status(400).send({ err: `Slug ${req.body.slug} is available. Please choose another unique slug` });
+    }
+
+    const mainCategory = await Category.findById(req.body.mainCategory);
+    if (!mainCategory) {
+      return res.status(400).send({ err: 'main category not found' });
+    }
+
+    if (req.body.parentCategories) {
+      if (!Array.isArray(req.body.parentCategories)) {
+        return res.status(400).send({ err: 'Parent category must be a array type' });
+      }
+      for await (let id of req.body.parentCategories) {
+        const category = await Category.findById(id);
+        if (!category) {
+          return res.status(400).send({ err: `Parent category ${id} not fount` });
+        }
+      }
+      req.body.parentCategories.push(mainCategory._id);
+    }
+
+    if (!req.body.parentCategories) {
+      req.body.parentCategories = [mainCategory._id];
+    }
+
+    //create new item
+    let itemUrl = [];
+    let currentMainCategory = mainCategory;
+
+    while (currentMainCategory.mainCategory) {
+      itemUrl.unshift(currentMainCategory.slug);
+      currentMainCategory = await Category.findById(currentMainCategory.mainCategory);
+    }
+
+    itemUrl.unshift('/' + currentMainCategory.slug);
+    itemUrl.push(req.body.slug);
+
+    const objItem = {
+      title: req.body.title,
+      slug: req.body.slug,
+      description: req.body.description,
+      html: req.body.html,
+      metaTags: req.body.metaTags,
+      mainCategory: req.body.mainCategory,
+      url: itemUrl.join('/')
+    }
+
+    const item = new Item(objItem);
+
+    await item.save();
+
+    await Category.findByIdAndUpdate(
+      mainCategory._id,
+      { $addToSet: { mainItems: item._id } },
+      { new: true, useFindAndModify: false }
+    );
+
+    addItemToCategory(req.body.parentCategories, item._id);
+    addCategoryToItem(req.body.parentCategories, item._id);
+
+    return res.status(201).send(item);
   } catch (e) {
-    res.status(400).send({ err: 'Please check required fields', message: e });
+    return res.status(500).send({ err: 'Please check required fields', message: e });
   }
 });
 
